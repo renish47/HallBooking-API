e826487fbe350cff969ab61f9918dfa8f674a2a5
renish47 <ineroff77@gmail.com>
2023-05-27 09:44:27 +0530

    update hall schema

--- model/hall.js
+++ model/hall.js
@@ -24,7 +24,7 @@ const hallSchema = new Schema(
                 type: Boolean,
                 default: false
             },
-            aoundSystem: {
+            soundSystem: {
                 type: Boolean,
                 default: false
             },
